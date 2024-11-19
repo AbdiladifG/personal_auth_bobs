ba8e35a7e5c5f041dc1d55fff073a954496dba9e
abdiladifgurhan <abdiladifgurhan@gmail.com>
2024-11-18 20:34:23 -0500

    first commit

---
+++ app/routes.js
@@ -0,0 +1,151 @@
+const { get } = require('lodash');
+
+module.exports = function(app, passport, db) {
+
+// normal routes ===============================================================
+    const axios = require('axios')
+    async function getCharacters() {
+      try {
+        const response = await axios.get('https://bobsburgers-api.herokuapp.com/characters/?limit=40&skip=6');
+        return response.data
+      } catch (error) {
+        throw error
+      }
+    }
+    let characters
+    getCharacters()
+    .then(data => {
+      characters = data
+    })
+    .catch(error => console.error(error));
+    // show the home page (will also have our login links)
+    app.get('/', function(req, res) {
+        res.render('index.ejs');
+    });
+
+    // PROFILE SECTION =========================
+    app.get('/profile', isLoggedIn, function(req, res) {
+        db.collection('messages').find().sort({thumbUp: -1}).toArray((err, result) => {
+
+          if (err) return console.log(err)
+          res.render('profile.ejs', {
+            name: req.name,
+            img: req.img,
+            characters: characters,
+            messages: result
+          })
+        })
+    });
+
+    // LOGOUT ==============================
+    app.get('/logout', function(req, res) {
+        req.logout(() => {
+          console.log('User has logged out!')
+        });
+        res.redirect('/');
+    });
+
+// message board routes ===============================================================
+
+    app.post('/favorite', (req, res) => {
+      db.collection('messages').insertOne({name: req.body.name, img: req.body.img, thumbUp: 0}, (err, result) => {
+        if (err) return console.log(err)
+        console.log('saved to database')
+        res.redirect('/profile')
+      })
+    })
+    
+
+    app.put('/messages', (req, res) => {
+      db.collection('messages')
+      .findOneAndUpdate({name: req.body.name}, {
+        $set: {
+          thumbUp:req.body.thumbUp + 1
+        }
+      }, {
+        sort: {_id: -1},
+        upsert: true
+      }, (err, result) => {
+        if (err) return res.send(err)
+        res.send(result)
+      })
+    })
+    app.put('/thumbDown', (req, res) => {
+      db.collection('messages')
+      .findOneAndUpdate({name: req.body.name}, {
+        $set: {
+          thumbUp:req.body.thumbUp - 1
+        }
+      }, {
+        sort: {_id: -1},
+        upsert: true
+      }, (err, result) => {
+        if (err) return res.send(err)
+        res.send(result)
+      })
+    })
+
+    app.delete('/messages', (req, res) => {
+      db.collection('messages').findOneAndDelete({name: req.body.name}, (err, result) => {
+        if (err) return res.send(500, err)
+        res.send('Message deleted!')
+      })
+    })
+
+// =============================================================================
+// AUTHENTICATE (FIRST LOGIN) ==================================================
+// =============================================================================
+
+    // locally --------------------------------
+        // LOGIN ===============================
+        // show the login form
+        app.get('/login', function(req, res) {
+            res.render('login.ejs', { message: req.flash('loginMessage') });
+        });
+
+        // process the login form
+        app.post('/login', passport.authenticate('local-login', {
+            successRedirect : '/profile', // redirect to the secure profile section
+            failureRedirect : '/login', // redirect back to the signup page if there is an error
+            failureFlash : true // allow flash messages
+        }));
+
+        // SIGNUP =================================
+        // show the signup form
+        app.get('/signup', function(req, res) {
+            res.render('signup.ejs', { message: req.flash('signupMessage') });
+        });
+
+        // process the signup form
+        app.post('/signup', passport.authenticate('local-signup', {
+            successRedirect : '/profile', // redirect to the secure profile section
+            failureRedirect : '/signup', // redirect back to the signup page if there is an error
+            failureFlash : true // allow flash messages
+        }));
+
+// =============================================================================
+// UNLINK ACCOUNTS =============================================================
+// =============================================================================
+// used to unlink accounts. for social accounts, just remove the token
+// for local account, remove email and password
+// user account will stay active in case they want to reconnect in the future
+
+    // local -----------------------------------
+    app.get('/unlink/local', isLoggedIn, function(req, res) {
+        var user            = req.user;
+        user.local.email    = undefined;
+        user.local.password = undefined;
+        user.save(function(err) {
+            res.redirect('/profile');
+        });
+    });
+
+};
+
+// route middleware to ensure user is logged in
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+
+    res.redirect('/');
+}
---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://abdiladifgurhan:KpiS1xf3cm4IGNPX@cluster0.6t2kdm9.mongodb.net/bobs?retryWrites=true&w=majority&appName=Cluster0', 
+    'dbName': 'bobs'
+};
